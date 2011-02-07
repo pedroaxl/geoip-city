417e201ceff61b629fb01603322018ab53ea533b
Juan Maiz <juanmaiz@gmail.com>
2011-02-07 00:30:22 -0200

    explaining a little about this crap

--- extconf.rb
+++
@@ -1,9 +0,0 @@
-require 'mkmf'
-
-dir_config("geoip")
-
-if have_library('GeoIP', 'GeoIP_record_by_ipnum') and have_header('GeoIPCity.h')
-  create_makefile('geoip_city')
-else
-  abort("you must have geoip c library installed!")
-end
